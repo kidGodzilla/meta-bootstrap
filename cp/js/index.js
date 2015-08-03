58a873ac03873859eb5ec2d0622531019a396581
James Futhey <futhey@gmail.com>
2015-08-03 16:32:27 -0700

    Ups

---
+++ cp/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('.featurekit-underlay, .featurekit-close-button, .featurekit-ok-button, .featurekit-hide-later').click(function() {
+    $('.featurekit-underlay, .featurekit-new-features-container').fadeOut();
+  });
+});
